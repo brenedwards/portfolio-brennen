879e602b92fbf2b61dc5f3b7784160062b4dacc9
JOYSTROMBECK <160427981+brenedwards@users.noreply.github.com>
2024-02-22 22:55:03 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 
-/*Intro animation*/
 const intro = document.querySelector(".intro");
 const logo = document.querySelector(".logo-header");
 const logoSpan = document.querySelectorAll(".logo");
@@ -25,7 +24,6 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 2000);
 });
 
-/*Show menu*/
 const showMenu = (toggleId, navID) => {
   const toggle = document.getElementById(toggleId);
   nav = document.getElementById(navID);
@@ -39,31 +37,31 @@ const showMenu = (toggleId, navID) => {
 
 showMenu("nav-toggle", "nav-menu");
 
-/*Active and remove menu*/
+
 const navLink = document.querySelectorAll(".nav_link");
 
 function linkAction() {
   navLink.forEach((n) => n.classList.remove("active"));
   this.classList.add("active");
 
-  /*This is for removing menu on click on mobile*/
+ 
   const navMenu = document.getElementById("nav-menu");
   navMenu.classList.remove("show_menu");
 }
 
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*Auto write text*/
+
 
 var app = document.getElementById("autoWrite");
 
 var typewriter = new Typewriter(app, {
-  strings: ["Customer Success", "Account Management", "Business Development", "Account Executive"],
+  strings: ["Customer Success", "Account Manager", "Business Development", "Account Executive"],
   loop: true,
   autoStart: true,
 });
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+
 const sr = ScrollReveal({
   origin: "top",
   distance: "40px",
@@ -71,25 +69,23 @@ const sr = ScrollReveal({
   reset: true,
 });
 
-/*SCROLL HOME*/
 sr.reveal(".home_title", {});
 sr.reveal(".button", { delay: 200 });
 sr.reveal(".home_img", { delay: 400 });
 sr.reveal(".home_social-icon", { interval: 200 });
 
-/*SCROLL ABOUT*/
+
 sr.reveal(".about_img", {});
 sr.reveal(".about_subtitle", { delay: 400 });
 sr.reveal(".about_text", { delay: 400 });
 
 
-/*SCROLL WORK*/
+
 sr.reveal(".work_img", { interval: 200 });
 
-/*SCROLL CONTACT*/
 sr.reveal(".contact_input", { interval: 200 });
 
-/*Toggle dark mode*/
+
 const toggleDarkMode = document.getElementById("toggle");
 
 toggleDarkMode.addEventListener("change", () => {
@@ -99,6 +95,6 @@ toggleDarkMode.addEventListener("change", () => {
 document.body.classList.toggle("dark");
 document.getElementById("head").classList.toggle("dark");
 
-/*Particles.js*/
+
 particlesJS.load("particles-js", "js/particles.json");
 
